afe61e23d54606d587f208746915cd7f61fd9d00
otix <otix@yoga14s>
2026-06-30 13:57:03 +0800

    fix(valid-parentheses): 修复栈为空时调用 pop_back 的未定义行为

---
+++ valid-parentheses/solution.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  bool isValid(string s) {
+    vector<char> stack;
+    for (char c : s) {
+      switch (c) {
+      case '(':
+      case '[':
+      case '{':
+        stack.push_back(c);
+        break;
+      default:
+        if (stack.empty()) return false;
+        auto p = stack.back();
+        stack.pop_back();
+        if (!((p == '{' && c == '}') || (p == '(' && c == ')') ||
+              (p == '[' && c == ']'))) {
+          return false;
+        }
+      }
+    }
+    if (stack.size() != 0) {
+      return false;
+    }
+    return true;
+  }
+};
+
+int main() {
+  Solution s;
+  cout << s.isValid("{{}}{{}}()()[][]") << endl;
+  cout << s.isValid("{{[[]}}") << endl;
+}
